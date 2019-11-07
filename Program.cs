f4034c4cc41281774f3d129bc63a266819da58c7
Liljoe5525 <56942177+Liljoe5525@users.noreply.github.com>
2019-11-06 19:42:45 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace joe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {// This is the product of 2 numbers
+            string str1, str2;
+
+            str1 = "Hello World ";
+            str2 = "Joe";
+
+            Console.WriteLine(str1 + " from " + str2);
+
+
+          
+
+        }
+
+    }
+}
